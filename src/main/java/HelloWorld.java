6df588ee4a9c20833ffc2544d644aa2150708849
yogananda-fs <yogananda.kanchisamudram@firstgroup.com>
2025-03-04 12:54:18 +0530

    Some

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void helloWorlds()
     {
-        System.out.println("Hello World from class Hello World new commit inton Repo B 2");
+        System.out.println("Hello World from class Hello World new commit inton RepoA1");
     }
 }
